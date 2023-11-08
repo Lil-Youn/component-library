ddcef44a0ddfebbd56024ad02601c3d02fa43f07
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-11-08 13:58:01 +0100

    Delete unused site pages.

--- src/pages/site1.tsx
+++
@@ -1,9 +0,0 @@
-function Site1() {
-  return (
-    <>
-      <h1>Site 1</h1>
-    </>
-  );
-}
-
-export default Site1;
--- src/pages/site2.tsx
+++
@@ -1,9 +0,0 @@
-function Site2() {
-  return (
-    <>
-      <h1>Site 1</h1>
-    </>
-  );
-}
-
-export default Site2;
