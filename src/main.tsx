96f02cf4d8cbc44505b62626ec7bf2da89e318eb
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-11-08 11:40:37 +0100

    Add react-query and react-router-dom to App component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./App.css";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>
 );
