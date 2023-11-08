aad57e137f4c9d7418499c839143eeeef6c98122
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-11-08 11:40:31 +0100

    Add react-router-dom and update App component to use it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Site1 from "./pages/site1";
+import Site2 from "./pages/site2";
+import Site3 from "./pages/site3";
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Routes location={location}>
+        <Route path="/" element={<Home />} />
+        <Route path="/site1" element={<Site1 />} />
+        <Route path="/site2" element={<Site2 />} />
+        <Route path="/site3" element={<Site3 />} />
+      </Routes>
     </>
   );
 }
