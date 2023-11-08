a2606f6ad85e0ca95f7b9d95ca750821188dfdcb
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-11-08 13:56:12 +0100

    Update page routes to match new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
-import Site1 from "./pages/site1";
-import Site2 from "./pages/site2";
-import Site3 from "./pages/site3";
+import Site1 from "./pages/buttons";
+import Site2 from "./pages/navbars";
+import Site3 from "./pages/cards";
+import Site4 from "./pages/typography";
+import Site5 from "./pages/menu";
 
 function App() {
   return (
     <>
       <Routes location={location}>
         <Route path="/" element={<Home />} />
-        <Route path="/site1" element={<Site1 />} />
-        <Route path="/site2" element={<Site2 />} />
-        <Route path="/site3" element={<Site3 />} />
+        <Route path="/buttons" element={<Site1 />} />
+        <Route path="/navbars" element={<Site2 />} />
+        <Route path="/cards" element={<Site3 />} />
+        <Route path="/typography" element={<Site4 />} />
+        <Route path="/menu" element={<Site5 />} />
       </Routes>
     </>
   );
