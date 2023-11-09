472c039258c17a71697336bcc8e1e0147ab4c98c
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-11-09 14:33:42 +0100

    Add info button to home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,7 @@ import ButtonLogo from "../assets/logo svgs/button.svg";
 import CardLogo from "../assets/logo svgs/cards.svg";
 import NavbarLogo from "../assets/logo svgs/navbars.svg";
 import TypographyLogo from "../assets/logo svgs/text.svg";
+import InfoLogo from "../assets/logo svgs/info.svg";
 import LibraryLogo from "/public/library.svg";
 import MenuLogo from "../assets/logo svgs/menu.svg";
 import { Link } from "react-router-dom";
@@ -22,15 +23,20 @@ function Home() {
 
   return (
     <Container>
-      <Box sx={{ my: 2, display: "flex", justifyContent: "flex-end" }}>
-        <IconButton
-          color="inherit"
-          onClick={handleInfoButtonClick}
-          sx={{ backgroundColor: "orange", borderRadius: "50%" }}
-        >
-          <InfoIcon />
-        </IconButton>
-      </Box>
+      <IconButton
+        color="inherit"
+        onClick={handleInfoButtonClick}
+        sx={{
+          backgroundColor: "orange",
+          borderRadius: "50%",
+          top: "10px",
+          right: "10px",
+          animation: "pulsate 2.5s infinite",
+        }}
+      >
+        <img src={InfoLogo} alt="logo" style={{ width: "1.5rem" }} />
+      </IconButton>
+
       <img className="site-logo" src={LibraryLogo} alt="logo" />
 
       <Box sx={{ my: 2 }}>
